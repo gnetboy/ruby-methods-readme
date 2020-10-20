6ace834cf32706c0faeb4fbf6c00a724cc21d697
Gustavo <gnetboy.y@gmail.com>
2020-10-19 19:11:04 -0700

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,13 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times
+    phrase = "Hello World!"
+    10.times do
+        puts "-"*12
+        puts phrase
+    end
+end
+
+say_hello_world_ten_times
